394984d93e6a50cfcef2cca89b948f62503720fb
lalo <jgarcia24121996@gmail.com>
2021-01-14 18:43:37 -0500

    reestructure folders template

--- CSSF/views.py
+++ CSSF/views.py
@@ -5,24 +5,24 @@ from django.shortcuts import render
 from CSSF.models import Categoria, Facultad
 
 def mainIndex(request):
-    return render(request, 'index.html')
+    return render(request, 'rp/homerp.html')
 
 def bodIndex(request):
-    return render(request, 'indexBod.html')
+    return render(request, 'bdg/indexbdg.html')
 
 def labIndex(request):
-    return render(request, 'indexLab.html')
+    return render(request, 'tl/indexLab.html')
 
 def myfirstview(request):
     data = {
         'name': 'Jose',
         'categoria': Categoria.objects.all()
     }
-    return render(request, 'indexprueba.html', data)
+    return render(request, 'prueba/indexprueba.html', data)
 
 def mysecondview(request):
     data = {
         'name': 'Jose',
         'facultad': Facultad.objects.all()
     }
-    return render(request, 'prueba.html', data)
+    return render(request, 'prueba/prueba.html', data)
