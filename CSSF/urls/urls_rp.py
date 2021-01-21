a7a8a5f6a309ae468e0a973d63facf5f2b8a84b8
lalo <jgarcia24121996@gmail.com>
2021-01-21 12:40:57 -0500

    add navigation pages

--- CSSF/urls/urls_rp.py
+++ CSSF/urls/urls_rp.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from CSSF.views.rp.views_rp import mainIndex
+from CSSF.views.rp.views_rp import *
 
 app_name = "rp"
 
 urlpatterns = [
     path('index/', mainIndex, name="index"),
+    path('inventario/', listarstocksustancias, name="inventario"),
+    path('inventario/movimientos/', listarmovimientoinventario, name="movimientoinventario"),
+    path('compras/', listarcompras, name="compras"),
+    path('compras/registro/', registrarcompra, name="registrocompras"),
+    path('solicitudes/', listarsolicitudesentregasustancias, name="listadosolicitudes"),
+    path('solicitudes/registro/', registrarsolicitidentregasustancias, name="entregasustancias")
 ]
--- CSSF/views/rp/views_rp.py
+++ CSSF/views/rp/views_rp.py
@@ -8,6 +8,114 @@ def mainIndex(request):
                 "uriname": "Home"
             }
         ],
-        "usertitle": "Representante Técnico"
+        "pagetitle": "Representante Técnico"
     }
     return render(request, 'rp/homerp.html', data)
+
+def listarmovimientoinventario(request):
+    data = {
+        "urls": [
+            {
+                "uridj": "rp:index",
+                "uriname": "Home"
+            },
+            {
+                "uridj": "rp:inventario",
+                "uriname": "Inventario"
+            },
+            {
+                "uridj": "rp:movimientoinventario",
+                "uriname": "Movimientos"
+            }
+        ],
+        "pagetitle": "Movimientos inventario"
+    }
+    return render(request, "rp/listarmovimientosinventario.html", data)
+
+def listarstocksustancias(request):
+    data = {
+        "urls": [
+            {
+                "uridj": "rp:index",
+                "uriname": "Home"
+            },
+            {
+                "uridj": "rp:inventario",
+                "uriname": "Inventario"
+            }
+        ],
+        "pagetitle": "Inventario"
+    }
+    return render(request, "rp/listarstocksustancias.html", data)
+
+def registrarcompra(request):
+    data = {
+        "urls": [
+            {
+                "uridj": "rp:index",
+                "uriname": "Home"
+            },
+            {
+                "uridj": "rp:compras",
+                "uriname": "Compras"
+            },
+            {
+                "uridj": "rp:registrocompras",
+                "uriname": "Registro"
+            }
+        ],
+        "pagetitle": "Registrar compra"
+    }
+    return render(request, "rp/registrocompras.html", data)
+
+def listarcompras(request):
+    data = {
+        "urls": [
+            {
+                "uridj": "rp:index",
+                "uriname": "Home"
+            },
+            {
+                "uridj": "rp:compras",
+                "uriname": "Compras"
+            }
+        ],
+        "pagetitle": "Compras registradas"
+    }
+    return render(request, "rp/listarcompras.html", data)
+
+def registrarsolicitidentregasustancias(request):
+    data = {
+        "urls": [
+            {
+                "uridj": "rp:index",
+                "uriname": "Home"
+            },
+            {
+                "uridj": "rp:listadosolicitudes",
+                "uriname": "Solicitudes"
+            },
+            {
+                "uridj": "rp:entregasustancias",
+                "uriname": "Registro"
+            }
+        ],
+        "pagetitle": "Registro entrega sustancias"
+    }
+    return render(request, "rp/solicitudentregasustancias.html", data)
+
+def listarsolicitudesentregasustancias(request):
+    data = {
+        "urls": [
+            {
+                "uridj": "rp:index",
+                "uriname": "Home"
+            },
+            {
+                "uridj": "rp:listadosolicitudes",
+                "uriname": "Solicitudes"
+            }
+        ],
+        "pagetitle": "Solicitudes resgistradas"
+    }
+    return render(request, "rp/listarsolicitudesentregasustancias.html", data)
